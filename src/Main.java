53b326a91a872b4c1cfb5fe7caff9c65058058c2
kaoskarlsson <89735425+kaoskarlsson@users.noreply.github.com>
2023-11-20 15:07:34 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,12 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
+
+
+
+
+
+        
         System.out.printf("Hello and welcome!");
 
         // Press Skift+F10 or click the green arrow button in the gutter to run the code.
