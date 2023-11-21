bab83196eb97e4bf403996a99453d7547a6f4328
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2023-11-21 14:02:09 +0100

    Update FrameLayout.java

--- src/FrameLayout.java
+++ src/FrameLayout.java
@@ -10,6 +10,10 @@ public class FrameLayout implements ActionListener {
     JLabel label = new JLabel();
     JPanel title_panel = new JPanel();
     JPanel button_panel = new JPanel();
+    JPanel south_panel = new JPanel();
+    JButton oneplayer_button = new JButton();
+    JButton twoplayer_button = new JButton();
+    JButton reset_button = new JButton();
     JButton[] buttons = new JButton[9];
 
     FrameLayout(){
@@ -29,6 +33,26 @@ public class FrameLayout implements ActionListener {
         title_panel.setLayout(new BorderLayout());
         title_panel.setBounds(0,0,700,90);
 
+        //Southpanel here , will add one player and two player button and reset button
+        oneplayer_button.setPreferredSize(new Dimension(150,100));
+        oneplayer_button.setFont(new Font("Monospaced",Font.BOLD,15));
+        oneplayer_button.setText("1 Player");
+
+        twoplayer_button.setPreferredSize(new Dimension(150,100));
+        twoplayer_button.setFont(new Font("Monospaced",Font.BOLD,15));
+        twoplayer_button.setText("2 Player");
+
+        reset_button.setPreferredSize(new Dimension(150,100));
+        reset_button.setFont(new Font("Monospaced", Font.BOLD,15));
+        reset_button.setText("Reset");
+
+        south_panel.setSize(700,100);
+        south_panel.setLayout(new FlowLayout());
+        south_panel.setBackground(Color.BLACK);
+        south_panel.add(oneplayer_button);
+        south_panel.add(twoplayer_button);
+        south_panel.add(reset_button);
+
         title_panel.add(textfield);
         frame.add(title_panel, BorderLayout.NORTH);
 
@@ -44,8 +68,31 @@ public class FrameLayout implements ActionListener {
 
         }
 
-
+        frame.add(title_panel, BorderLayout.NORTH);
         frame.add(button_panel);
+        frame.add(south_panel, BorderLayout.SOUTH);//Adds southpanel to frame
+
+        oneplayer_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Du tryckte på one player knappen");
+
+            }
+        });
+        twoplayer_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Du tryckte på two player knappen");
+
+            }
+        });
+        reset_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Du tryckte på reset knappen");
+
+            }
+        });
 
 
 
