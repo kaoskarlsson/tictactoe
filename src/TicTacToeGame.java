e700d4a829495332bf80cba05b45351e9e380a51
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2023-11-23 22:16:59 +0100

    Update TicTacToeGame.java

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,36 +1,25 @@
-import javax.swing.*; // Innehåller klasser och metoder för att skapa GUI-komponenter
-import java.awt.*;  // Innehåller klasser för GUI-komponenter och layouthantering
-import java.awt.event.ActionEvent;  // Representerar en händelse som skapas när en användaråtgärd utförs
-import java.awt.event.ActionListener;  // Ett gränssnitt för att lyssna på händelser av typen ActionEvent.
-import java.util.Random;  // Används för att generera slumpmässiga tal
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-// Klassen TicTacToeGame implementerar ActionListener för att hantera händelser från buttons
 public class TicTacToeGame implements ActionListener {
 
-    // En instans av Random för att hantera slumpmässiga val
     Random random = new Random();
-    // Fönstret för spelet
     JFrame frame = new JFrame();
-    // Visar meddelanden och spelstatus
     JTextField textfield = new JTextField();
     JLabel label = new JLabel();
-    // JPanel för titeln
     JPanel title_panel = new JPanel();
-    // JPanel för spelknapparna
     JPanel button_panel = new JPanel();
-
     JPanel south_panel = new JPanel();
-    // Knappar för olika spelalternativ
     JButton oneplayer_button = new JButton();
     JButton twoplayer_button = new JButton();
     JButton reset_button = new JButton();
-    // Array för spelknapparna
     JButton[] buttons = new JButton[9];
-    // Variabel för att hålla reda på vilken spelares tur det är
     boolean player1_turn;
     boolean twoPlayer_Mode;
 
-    // Konstruktor för att initialisera och sätta upp spelet
     TicTacToeGame() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
@@ -38,7 +27,6 @@ public class TicTacToeGame implements ActionListener {
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
 
-        // Inställningar för textfält som visar meddelanden och spelstatus
         textfield.setBackground(Color.BLACK);
         textfield.setForeground(Color.CYAN);
         textfield.setFont(new Font("Monospaced", Font.PLAIN, 20));
@@ -46,91 +34,94 @@ public class TicTacToeGame implements ActionListener {
         textfield.setText("Tic-Tac-Toe");
         textfield.setOpaque(true);
 
-        // Inställningar för titel_panel
-        title_panel.setLayout(new BorderLayout());
-        title_panel.setBounds(0, 0, 800, 100);
-
-        // Inställningar för title_panel
         title_panel.setLayout(new BorderLayout());
         title_panel.setPreferredSize(new Dimension(800, 50));
 
         button_panel.setLayout(new GridLayout(3, 3));
         button_panel.setBackground(new Color(150, 150, 150));
 
-        disableAllButtons();
-
+        //----------new Add buttons to southpanel panel------------>
         south_panel.setLayout(new FlowLayout());
         south_panel.setPreferredSize(new Dimension(800, 50));
         south_panel.setBackground(Color.BLACK);
+//-----------new add set text to buttons--------->
+        oneplayer_button.setText("1 Player");
+        twoplayer_button.setText("2 Player");
+        reset_button.setText("Reset");
 
+        //-----------new add-------------
+        south_panel.add(oneplayer_button);
+        south_panel.add(twoplayer_button);
+        south_panel.add(reset_button);
+
+        //----new add actionlistener to buttons---------
         oneplayer_button.addActionListener(this);
         twoplayer_button.addActionListener(this);
         reset_button.addActionListener(this);
 
-        // Skapar och lägger till knappar i button_panel
+
         for (int i = 0; i < 9; i++) {
             buttons[i] = new JButton();
             button_panel.add(buttons[i]);
             buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
+            buttons[i].setBackground(Color.LIGHT_GRAY);
             buttons[i].setFocusable(false);
             buttons[i].addActionListener(this);
         }
 
-        // Lägger till textfält i titel_panel och titel_panel i frame
-        //title_panel.add(textfield);//
-        //frame.add(title_panel,BorderLayout.NORTH);//
-        // Lägger till button_panel i frame
-        //frame.add(button_panel);//
-        //frame.add(south_panel, BorderLayout.SOUTH);//
+        title_panel.add(textfield);
+        frame.add(title_panel, BorderLayout.NORTH);
+        frame.add(button_panel);
+        //---------new add frame add-------
+        frame.add(south_panel, BorderLayout.SOUTH);
+
+        disableAllButtons();
 
         frame.revalidate();
         frame.repaint();
 
-        // Anropar firstTurn för att bestämma vilken spelare som börjar
-        //firstTurn();//
 
     }
 
-    // Metoden för att bestämma vilken spelare som börjar spelet
     private void firstTurn() {
-        // Random för att bestämma om det är X eller O som börjar
-        if (random.nextInt(2) == 0) {
+        if (twoPlayer_Mode==false){
             player1_turn = true;
             textfield.setText("X turn");
-        } else {
-            player1_turn = false;
-            textfield.setText("O turn");
+        } else if (twoPlayer_Mode==true) {
+            if (random.nextInt(2) == 0) {
+                player1_turn = true;
+                textfield.setText("X turn");
+            } else {
+                player1_turn = false;
+                textfield.setText("O turn");
+            }
         }
+
     }
 
-    // Metoden som hanterar händelser från knapptryckningar
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Loopar igenom spelknapparna för att avgöra vilken knapp som tryckts
-        //for(int i=0;i<9;i++) {
-
-        // Hanterar knapptryckningen beroende på vilken spelare som är näst på tur
-        //if(e.getSource()==buttons[i]) {
-        //  if(player1_turn) {
-        //    if(buttons[i].getText()=="") {
-        //
-        //      buttons[i].setForeground(new Color(255,0,0));
 
         if (e.getSource() == oneplayer_button) {
+
             System.out.println("Du tryckte på 1 player knappen");
             twoPlayer_Mode = false;
-            resetButtons();
+            one_player();
+           // resetButtons();
             // enableButtons();
         } else if (e.getSource() == twoplayer_button) {
+
             System.out.println("Du tryckte på 2 player knappen");
             twoPlayer_Mode = true;
             resetButtons();
+
             //-------Add new first turn button here instead---------
             firstTurn();
         }
         //----------Adds new reset button-----------
         else if (e.getSource() == reset_button) {
             System.out.println("Du tryckte på reset button");
+
             resetButtons();
             firstTurn();
         }
@@ -140,10 +131,8 @@ public class TicTacToeGame implements ActionListener {
                     if (buttons[i].getText() == "") {
                         buttons[i].setForeground(new Color(255, 0, 0));
                         buttons[i].setText("X");
-
                         player1_turn = false;
                         textfield.setText("O turn");
-                        // Kontrollerar om någon har vunnit eller om det är oavgjort
                         check();
                         checkTie();
                     }
@@ -153,18 +142,18 @@ public class TicTacToeGame implements ActionListener {
                         buttons[i].setText("O");
                         player1_turn = true;
                         textfield.setText("X turn");
-                        // Kontrollerar om någon har vunnit eller om det är oavgjort
                         check();
                         checkTie();
                     }
                 }
             }
         }
+        //Infoga Try catch här? //
     }
 
-    // Metod för att kontrollera om någon har vunnit eller om det är oavgjort
+
     public void check() {
-        // Kontrollerar X-vinster i rader, kolumner och diagonaler
+        //check X win conditions
         if (
                 (buttons[0].getText() == "X") &&
                         (buttons[1].getText() == "X") &&
@@ -186,6 +175,7 @@ public class TicTacToeGame implements ActionListener {
         ) {
             xWins(6, 7, 8);
         }
+
 
         if (
                 (buttons[0].getText() == "X") &&
@@ -216,6 +206,7 @@ public class TicTacToeGame implements ActionListener {
             xWins(0, 4, 8);
         }
 
+
         if (
                 (buttons[2].getText() == "X") &&
                         (buttons[4].getText() == "X") &&
@@ -223,8 +214,7 @@ public class TicTacToeGame implements ActionListener {
         ) {
             xWins(2, 4, 6);
         }
-
-        // Kontrollerar O-vinster i rader, kolumner och diagonaler
+        //check O win conditions
         if (
                 (buttons[0].getText() == "O") &&
                         (buttons[1].getText() == "O") &&
@@ -283,38 +273,35 @@ public class TicTacToeGame implements ActionListener {
         }
     }
 
-    // Metoden som kallas när X vinner, markerar vinnande celler och inaktiverar knappar
     public void xWins(int a, int b, int c) {
         buttons[a].setBackground(Color.GREEN);
         buttons[b].setBackground(Color.GREEN);
         buttons[c].setBackground(Color.GREEN);
 
-        // Inaktiverar alla knappar
         for (int i = 0; i < 9; i++) {
             buttons[i].setEnabled(false);
         }
-        // Visar meddelande att X har vunnit
         textfield.setText("X wins");
     }
 
-    // Metoden som kallas när O vinner, markerar vinnande celler och inaktiverar knappar
     public void oWins(int a, int b, int c) {
         buttons[a].setBackground(Color.GREEN);
         buttons[b].setBackground(Color.GREEN);
         buttons[c].setBackground(Color.GREEN);
 
-        // Inaktiverar alla knappar
         for (int i = 0; i < 9; i++) {
             buttons[i].setEnabled(false);
         }
         textfield.setText("O wins");
     }
 
-    // Metoden för att kontrollera om det är oavgjort
+
     private void checkTie() {
         boolean tie = true;
 
-        // Kollar om det finns några tomma celler kvar
+        // Kolla om det finns några tomma celler kvar
+
+        // JButton[] buttons = new JButton[0];
         for (JButton button : buttons) {
             if (button.getText().isEmpty()) {
                 tie = false;
@@ -322,10 +309,10 @@ public class TicTacToeGame implements ActionListener {
             }
         }
 
-        // Om ingen har vunnit och inga tomma celler finns kvar, är det oavgjort
+        // Om ingen vunnit och inga tomma celler finns kvar, är det oavgjort
         if (tie) {
             textfield.setText("It's a tie!");
-
+            //--------Set background color red if tie------
             for (int i = 0; i < 9; i++) {
                 buttons[i].setBackground(Color.RED);
             }
@@ -334,14 +321,22 @@ public class TicTacToeGame implements ActionListener {
         }
     }
 
-    // Metod för att inaktivera alla knappar
     private void disableAllButtons() {
         for (JButton button : buttons) {
             button.setEnabled(false);
         }
     }
 
+    //----------Add new method enable buttons---------------
+  /*  public void enableButtons(){
+        for(int i = 0; i < 9; i++){
+            buttons[i].setEnabled(twoPlayer_Mode);
+        }
+
+    }*/
+    //----------adds new reset button method here----------
     public void resetButtons() {
+
         for (int i = 0; i < 9; i++) {
             buttons[i].setText("");
             buttons[i].setBackground(Color.LIGHT_GRAY);
@@ -353,28 +348,24 @@ public class TicTacToeGame implements ActionListener {
         twoPlayer_Mode = false;
         player1_turn = true;
         textfield.setText("Game reset");
-
-        boolean twoPlayer_Mode = false;
-        player1_turn = true;
-        textfield.setText("Game reset");
     }
-}
-    /*
+
     public void one_player() {
         for (int i = 0; i < 9; i++) {
             //if (e.getSource() == buttons[i]) {
-            if (player1_turn) {
-                if (buttons[i].getText() == "") {
-                    buttons[i].setForeground(new Color(255, 0, 0));
-                    //random.nextInt(0,3);
-                    buttons[random.nextInt(0,9)].setText("X");
-                    player1_turn = false;
-                    textfield.setText("O turn");
-                    check();
-                    checkTie();
+                if (player1_turn) {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(255, 0, 0));
+                        //random.nextInt(0,3);
+                        buttons[random.nextInt(0, 9)].setText("X");
+                        player1_turn = false;
+                        textfield.setText("O turn");
+                        check();
+                        checkTie();
+
+                    }
                 }
-            }
-                else {
+                /*else {
                     if (buttons[i].getText() == "") {
                         buttons[i].setForeground(new Color(0, 0, 255));
                         //random.nextInt(0,3);
@@ -384,7 +375,10 @@ public class TicTacToeGame implements ActionListener {
                         check();
                         checkTie();
                     }
-                    */
+                }*/
 
+            }
+        }
+    }
 
 
